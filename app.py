8adb54c9192ac1fe1c38b85f71a6104f912155e2
tmdghlrla <tmdghlrla@google.com>
2024-01-18 14:20:10 +0900

    add

---
+++ app.py
@@ -0,0 +1,36 @@
+# import serverless_wsgi    # docker
+
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+from config import Config
+from resource.user import UserLoginResource, UserRegisterResource
+
+# 로그아웃 관련된 임포트문
+# from resources.user import jwt_blocklist
+
+app = Flask(__name__)
+
+# 환경변수 셋팅
+app.config.from_object(Config)
+# JWT 매니저 초기화
+jwt=JWTManager(app)
+
+# # 로그아웃된 토큰으로 요청하는 경우 실행되지 않도록 처리하는 코드
+# @jwt.token_in_blocklist_loader
+# def check_if_token_is_revoked(jwt_header, jwt_payload) :
+#     jti = jwt_payload['jti']
+#     return jti in jwt_blocklist
+
+api = Api(app)
+
+
+# 경로와 리소스를 연결한다.
+api.add_resource(UserRegisterResource,'/user/register') # 회원가입
+api.add_resource(UserLoginResource, '/user/login')      # 로그인
+api.add_resource(KakaoRegisterResource,'/user/register') # 카카오 회원가입
+# def handler(event, context) :
+#     return serverless_wsgi.handle_request(app, event, context)
+
+if __name__ == '__main__' :
+    app.run()
---
+++ resource/user.py
@@ -0,0 +1,110 @@
+from flask import request
+from flask_jwt_extended import create_access_token
+from flask_restful import Resource
+from mysql_connection import get_connection
+from mysql.connector import Error
+
+import requests
+from email_validator import EmailNotValidError, validate_email
+from utils import check_password, hash_password
+
+# 회원가입
+class UserRegisterResource(Resource) :
+    def post(self) :
+        data = request.get_json()        
+
+        # 이메일 유효성 검사
+        try :
+            validate_email(data['email'])
+
+        except EmailNotValidError as e :
+            print(e)
+            return {"Error" : str(e)}, 400
+
+        # 비밀번호 길이 검사
+        if len(data['password']) < 4 and len(data['password']) > 14 :
+            return {"Error" : "비밀번호 길이가 올바르지 않습니다."}, 400
+
+        # 단방향 암호화된 비밀번호를 저장
+        password = hash_password(data['password'])
+
+        try :
+            connection = get_connection()        
+
+            query = '''insert into user
+                    (nickName, email, password)
+                    value(%s, %s, %s);'''
+            record = (data['nickName'], data['email'], password)
+
+            cursor = connection.cursor()
+            cursor.execute(query, record)
+
+            connection.commit()
+
+            # 데이터베이스에서 받아온 유저 아이디 암호화
+            user_id = cursor.lastrowid
+
+            cursor.close()
+            connection.close()
+
+        except Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+
+            return {"Error" : str(e)}, 500
+        
+        # user 테이블의 id로 JWT 토큰을 만들어야 한다.
+        access_token = create_access_token(user_id)
+        
+        return {"result" : "success", "accessToken" : access_token}, 200
+
+# todo : 카카오 회원가입
+class KakaoRegisterResource(Resource) :
+    pass
+
+# 로그인
+class UserLoginResource(Resource) :
+    def post(self) :
+        data = request.get_json()
+        try :
+            connection = get_connection()
+
+            query = '''select id, nickName, email, password 
+                        from user
+                        where email = %s;'''
+            
+            record = (data['email'],)
+
+            # 딕셔너리 형태로 가져옴
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute(query, record)
+            result_list = cursor.fetchall()
+
+            cursor.close()
+            connection.close()
+
+        except Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+
+            return {"fail" : str(e)}, 500        
+
+        # 가입정보 확인
+        if len(result_list) == 0 :
+            return {"Error" : "회원가입된 정보가 없습니다."}, 400
+        
+        print()
+        print(result_list[0]['password'])
+        password = str(data['password'])
+        print()
+
+        check = check_password(password, result_list[0]['password'])
+        if check == False :
+            return {"error" : "비밀번호가 맞지 않습니다."}, 406
+        
+        # 암호화 토큰생성
+        access_token = create_access_token(result_list[0]['id'])
+
+        return {"result" : "success", "accessToken" : access_token}, 200
