89f0a740319e7ff7408c6336362f1feb87ccc3f5
tmdghlrla <tmdghlrla@google.com>
2024-02-19 10:44:46 +0900

    add

--- resources/ranker.py
+++ resources/ranker.py
@@ -20,7 +20,8 @@ class RankerResource(Resource):
         try :
             connection = get_connection()
             
-            query = '''select u.id, rank() over(order by level desc) as ranking, u.nickName, u.profileUrl, l.level
+            query = '''select u.id, rank() over(order by level desc) as ranking, 
+                            u.nickName, u.profileUrl, l.level, u.id as userId
                         from user u
                         join level l
                         on u.id = l.userId
